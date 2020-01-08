88392680d0bfdef65defbe364c299b9149ba730d
Zac Sweers <zsweers@slack-corp.com>
2020-01-08 15:38:49 -0500

    Add Names test

---
+++ sample/src/main/kotlin/dev/zacsweers/aak/sample/Names.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2019. Zac Sweers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.zacsweers.aak.sample
+
+import dev.zacsweers.aak.annotation.AutoAnnotation
+import kotlin.reflect.KClass
+
+annotation class Named(val name: String)
+annotation class NestedAnnotation(val name: String, val nested: Named)
+annotation class GenericAnnotation<T : Any>(val generic: KClass<T>)
+
+object Names {
+
+  @AutoAnnotation fun nameAnnotation(name: String): Named {
+    return aak_names_nameAnnotation(name)
+  }
+
+  @AutoAnnotation fun nestedAnnotation(name: String, named: Named): NestedAnnotation {
+    return aak_names_nestedAnnotation(name, named)
+  }
+
+  inline fun <reified T : Any> genericAnnotation(): GenericAnnotation<T> {
+    return genericAnnotation(T::class)
+  }
+
+  // Can't do reified because they're synthetic and therefore not visible from apt
+  @AutoAnnotation inline fun <T : Any> genericAnnotation(target: KClass<T>): GenericAnnotation<T> {
+    return aak_names_genericAnnotation(target)
+  }
+
+//  fun nameAnnotationDirect(name: String): Named {
+//    return Named::class.aakInstance(name)
+//  }
+}
+
+// fun aak_names_nameAnnotation(name: String): Named {
+//  val handler = InvocationHandler { proxy, method, args ->
+//    when (method.name) {
+//      "name" -> name
+//      else -> null
+//    }
+//  }
+//  return Proxy.newProxyInstance(Named::class.java.classLoader, arrayOf(Named::class.java), handler) as Named
+// }
---
+++ sample/src/test/kotlin/dev/zacsweers/aak/sample/NamesTest.kt
@@ -0,0 +1,42 @@
+package dev.zacsweers.aak.sample
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class NamesTest {
+
+  @Test
+  fun simpleString() {
+    val namedAnnotation = Names.nameAnnotation("taco")
+    assertThat(namedAnnotation.name).isEqualTo("taco")
+    assertThat(namedAnnotation.hashCode()).isEqualTo(0)
+    assertThat(namedAnnotation.toString()).isEqualTo("AakProxy_Names#nameAnnotation")
+    assertThat(namedAnnotation == namedAnnotation).isFalse()
+  }
+
+  @Test
+  fun nested() {
+    val nestedAnnotation = Names.nestedAnnotation("taco", Names.nameAnnotation("nestedTaco"))
+    assertThat(nestedAnnotation.name).isEqualTo("taco")
+    assertThat(nestedAnnotation.hashCode()).isEqualTo(0)
+    assertThat(nestedAnnotation.toString()).isEqualTo("AakProxy_Names#nestedAnnotation")
+    assertThat(nestedAnnotation == nestedAnnotation).isFalse()
+
+    val namedAnnotation = nestedAnnotation.nested
+    assertThat(namedAnnotation.name).isEqualTo("nestedTaco")
+    assertThat(namedAnnotation.hashCode()).isEqualTo(0)
+    assertThat(namedAnnotation.toString()).isEqualTo("AakProxy_Names#nameAnnotation")
+    assertThat(namedAnnotation == namedAnnotation).isFalse()
+  }
+
+  @Test
+  fun generic() {
+    val genericAnnotation = Names.genericAnnotation<String>()
+    // TODO this access results in a ClassCastException at runtime
+    //  kotlin.jvm.internal.ClassReference cannot be cast to java.lang.Class
+//    assertThat(genericAnnotation.generic.toString()).isEqualTo("")
+    assertThat(genericAnnotation.hashCode()).isEqualTo(0)
+    assertThat(genericAnnotation.toString()).isEqualTo("AakProxy_Names#genericAnnotation")
+    assertThat(genericAnnotation == genericAnnotation).isFalse()
+  }
+}
